49d477c2b57a48762a82a5aa662ce3b4b8ca8847
BlueBaBun <34680834+BlueBaBun@users.noreply.github.com>
2019-08-27 17:39:27 +0900

    DataRoomDTO.java
    08/27

---
+++ MVC2/Java Resouces/src/model.dataroom/DataRoomDTO.java
@@ -0,0 +1,64 @@
+package model.dataroom;
+
+public class DataRoomDTO {
+	private String no;
+	private String name;
+	private String title;
+	private String password;
+	private String content;
+	private java.sql.Date postDate;
+	private String attachedFile;
+	private String downCount;
+	
+	
+	//게터/세터
+	public String getNo() {
+		return no;
+	}
+	public void setNo(String no) {
+		this.no = no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public java.sql.Date getPostDate() {
+		return postDate;
+	}
+	public void setPostDate(java.sql.Date postDate) {
+		this.postDate = postDate;
+	}
+	public String getAttachedFile() {
+		return attachedFile;
+	}
+	public void setAttachedFile(String attachedFile) {
+		this.attachedFile = attachedFile;
+	}
+	public String getDownCount() {
+		return downCount;
+	}
+	public void setDownCount(String downCount) {
+		this.downCount = downCount;
+	}
+	
+}
