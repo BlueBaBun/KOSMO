6fcdc38f8fc57c26b80eaa1390bba09ccf4de49b
BlueBaBun <34680834+BlueBaBun@users.noreply.github.com>
2019-08-28 17:55:09 +0900

    WriteController.java
    08/28 수

--- MVC2/Java Resouces/src/controller.dataroom/WriteController.java
+++ MVC2/Java Resouces/src/controller.dataroom/WriteController.java
@@ -1,6 +1,7 @@
 package controller.dataroom;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -75,11 +76,54 @@ public class WriteController extends HttpServlet {
 			successOrFail = -1;
 		}
 		//5]리퀘스트 영역에 결과값 혹은 필요한 값 저장
-		req.setAttribute("SUCFAILL",successOrFail);
+		req.setAttribute("SUCFAIL",successOrFail);
 		//5-2]컨트롤러 구분용-입력:INS,수정:EDT,삭제:DEL
 		req.setAttribute("WHERE", "INS");
 		//6]뷰로 이동:성공시 바로 목록으로 ,성공이 아닐시 메시지를 뿌려주는 JSP페이지로 이동
-		req.getRequestDispatcher("/DataRoom/List.kosmo").forward(req, resp);
+		
+		if(successOrFail ==1) {//파일업로드 및 DB입력 성공
+			//[이동방법1]-바로 목록으로 이동:반드시 List.jsp가 아닌 List.kosmo로
+			//req.getRequestDispatcher("/DataRoom/List.kosmo").forward(req, resp);
+			//[이동방법2]-메시지 뿌려주는 페이지로 이동후 목록으로 이동
+			req.getRequestDispatcher("/DataRoom13/Message.jsp").forward(req, resp);
+		}
+		else {//파일 용량 초과 혹은 파일업로드는 됬으나 DB입력 실패한 경우
+			//[이동방법1]-자스의 history.back()으로 입력폼으로 이동
+			//서블릿에서 브라우저로 직접 출력시 -한글처리]
+			/*
+			resp.setContentType("text/html;charset=UTF-8");
+			PrintWriter out=resp.getWriter();
+			out.println("<script>");
+			out.println("alert('"+(successOrFail ==0 ?"DB입력 실패":"파일 용량 초과")+"');");
+			out.println("history.back();");
+			out.println("</script>");*/
+			//[이동방법2]메시지 뿌려주는 페이지로 이동후 다시 입력폼으로
+			req.getRequestDispatcher("/DataRoom13/Message.jsp").forward(req, resp);
+			
+			//이동방법3-비추천]비밀번호도 기존값 유지하기 위한 방법-역시 입력폼으로 이동
+			//※MultipartRequest가 파라미터를 가로채니까
+			//포워드하더라도 전달안됨 그래서 영역에 저장]
+			/*
+			if(mr == null) {
+				//"UTF-8"생략시 파라미터로 받은 한글이 깨짐]
+				mr= new MultipartRequest(
+						req,
+						req.getServletContext().getRealPath("/Upload"),"UTF-8");
+				
+				
+			}			
+			req.setAttribute("name", mr.getParameter("name"));
+			req.setAttribute("title", mr.getParameter("title"));
+			req.setAttribute("content", mr.getParameter("content"));
+			req.setAttribute("pass", mr.getParameter("pass"));
+			req.setAttribute("attachedfile",mr.getOriginalFileName("attachedfile"));
+			req.setAttribute("error", sucOrFail==0?"Input Failure":"Exceed File Size");
+			req.getRequestDispatcher("/DataRoom13/Write.jsp").forward(req, resp);
+			*/
+			
+			
+		}
+		
 	}///////////////////////doPost
 	
 	
